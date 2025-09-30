032e6370c350399fc22d34e4617876398156eda7
Pablo-wood <wisebuh3@gmail.com>
2025-09-30 13:13:17 +0100

    Database

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import  Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 
