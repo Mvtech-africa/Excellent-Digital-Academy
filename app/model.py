ca9f546b93bb7cb1b9f3e4a1299acb93246f97c4
Pablo-wood <wisebuh3@gmail.com>
2025-10-03 12:38:45 +0100

    Updated alot

--- app/model.py
+++ app/model.py
@@ -1,16 +1,16 @@
 from flask_login import UserMixin
-from app import db  # Absolute import from app package
+from app import db
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, String
-
+from sqlalchemy import Integer, String, Boolean
 
 class User(UserMixin, db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement="auto")
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     first_name: Mapped[str] = mapped_column(String(50), nullable=False)
     last_name: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(200), nullable=False)
     phone: Mapped[str] = mapped_column(String(20), nullable=False)
+    tos: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, email={self.email!r})"
