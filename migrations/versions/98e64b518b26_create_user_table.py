84cc3a55067abc54e2c9d2b480a1f1ce8c973edd
Pablo-wood <wisebuh3@gmail.com>
2025-10-03 17:46:14 +0100

    Update

---
+++ migrations/versions/98e64b518b26_create_user_table.py
@@ -0,0 +1,24 @@
+"""create User table
+
+Revision ID: 98e64b518b26
+Revises: 
+Create Date: 2025-10-03 17:31:57.014587
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '98e64b518b26'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
