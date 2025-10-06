00cfd46790d523eb332e34cc2c0cd975d9d56380
mvtech <mvtech44@gmail.com>
2025-10-06 15:12:40 +0100

    Updated Js

--- app/static/JS/main.js
+++ app/static/JS/main.js
@@ -546,20 +546,45 @@ slider.addEventListener('mouseleave', () => scrollInterval = setInterval(autoScr
 
 
 
-    // -------------------multi tab -----------------------
 
-    const tabs = document.querySelectorAll(".tab");
-    const contents = document.querySelectorAll(".tab-content");
 
-    tabs.forEach(tab => {
-      tab.addEventListener("click", () => {
-        // Remove active class from all
-        tabs.forEach(t => t.classList.remove("active"));
-        contents.forEach(c => c.classList.remove("active"));
 
-        // Add active class to clicked tab and related content
-        tab.classList.add("active");
-        document.getElementById(tab.dataset.tab).classList.add("active");
-      });
-    });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
